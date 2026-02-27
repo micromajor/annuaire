38c9dd57440d9c1f2e8c4c2dc6f2c6e6460facb6
dorian <dorian.pasturel@gmail.com>
2026-02-27 12:28:23 +0100

    chore: nettoyage scripts/tmp + ajout .venv et scripts/tmp/* au .gitignore

--- scripts/tmp/set_password.sql
+++
@@ -1,2 +0,0 @@
-UPDATE "Artisan" SET "passwordHash" = '$2b$10$Rz9YhRd3tom6P67rvDrq1Oc4u8eDiOsDzbxvMpyHA/0WYEEzOvzX2' WHERE email = 'b.gauthier@maconnerie44.fr';
-SELECT id, email, "passwordHash" IS NOT NULL as has_password FROM "Artisan" WHERE email = 'b.gauthier@maconnerie44.fr';
